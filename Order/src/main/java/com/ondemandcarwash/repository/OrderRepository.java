0e323b0dfc830406b6533023c6c7eed7ffe07bdf
vsabavat <vsabavat@lin33004807.corp.capgemini.com>
2021-12-09 14:22:37 +0530

    Order_Service

---
+++ Order/src/main/java/com/ondemandcarwash/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.ondemandcarwash.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.ondemandcarwash.model.Order;
+
+public interface OrderRepository extends MongoRepository<Order, Integer> {
+
+}
