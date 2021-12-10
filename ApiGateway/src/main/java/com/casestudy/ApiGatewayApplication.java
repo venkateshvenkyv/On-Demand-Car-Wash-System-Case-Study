f5945c11b4465487d84d05eda640ab95d4b48cac
vsabavat <vsabavat@lin33004807.corp.capgemini.com>
2021-12-11 01:13:36 +0530

    Second_Commit

---
+++ ApiGateway/src/main/java/com/casestudy/ApiGatewayApplication.java
@@ -0,0 +1,24 @@
+package com.casestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ApiGatewayApplication {
+
+	@Bean
+	public RestTemplate template()
+	{
+		return new RestTemplate();
+	}
+	
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
