267df5e2b082847a55a50ec607b9e106c93ee04b
vsabavat <vsabavat@lin33004807.corp.capgemini.com>
2022-01-24 09:01:35 +0530

    Final Commit

---
+++ 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/ApiGateway/src/main/java/com/casestudy/venky/ApiGateway/ApiGatewayApplication.java
@@ -0,0 +1,26 @@
+package com.casestudy.venky.ApiGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+public class ApiGatewayApplication {
+
+	@Bean
+	public RestTemplate template()
+	{
+		return new RestTemplate();
+	}
+	
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
